4faf50b1d90011d275456a3a80b2fef3c0f7260d
vasyukov-sv <vasukovsv@gmail.com>
2018-01-29 16:14:20 +0300

    -remove some

--- app/src/main/java/home/my/mypullup/obj/Attempt.java
+++ app/src/main/java/home/my/mypullup/obj/Attempt.java
@@ -1,28 +1,63 @@
 package home.my.mypullup.obj;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Attempt {
-    private int attempt1;
-    private int attempt2;
+    private int morning1;
+    private int morning2;
+    private int evening1;
+    private int evening2;
 
-    public Attempt(int attempt1, int attempt2) {
-        this.attempt1 = attempt1;
-        this.attempt2 = attempt2;
+    private String date;
+
+    public Attempt(int morning1, int morning2, int evening1, int evening2) {
+        this.morning1 = morning1;
+        this.morning2 = morning2;
+        this.evening1 = evening1;
+        this.evening2 = evening2;
+        this.date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
     }
 
-    public int getAttempt1() {
-        return attempt1;
+    public int getMorning1() {
+        return morning1;
     }
 
-    public void setAttempt1(int attempt1) {
-        this.attempt1 = attempt1;
+    public void setMorning1(int morning1) {
+        this.morning1 = morning1;
     }
 
-    public int getAttempt2() {
-        return attempt2;
+    public int getMorning2() {
+        return morning2;
     }
 
-    public void setAttempt2(int attempt2) {
-        this.attempt2 = attempt2;
+    public void setMorning2(int morning2) {
+        this.morning2 = morning2;
     }
 
+    public int getEvening1() {
+        return evening1;
+    }
+
+    public void setEvening1(int evening1) {
+        this.evening1 = evening1;
+    }
+
+    public int getEvening2() {
+        return evening2;
+    }
+
+    public void setEvening2(int evening2) {
+        this.evening2 = evening2;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
